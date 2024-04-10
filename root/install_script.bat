9254d5a01e845168d43a68a3fd4cf2efd29a3a44
mixelpixx <chris_stockslager@comcast.net>
2024-04-10 19:23:14 -0400

    Delete root directory

--- root/install_script.bat
+++
@@ -1,2 +0,0 @@
-- pyinstaller --onefile main.py
-+ pyinstaller --onefile --add-data "chroma_db;chroma_db" --add-data "docs;docs" main.py
--- root/start.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start /min cmd /C python main.py
-start /min cmd /C GUI\build\windows\runner\Release\app.exe
-exit
